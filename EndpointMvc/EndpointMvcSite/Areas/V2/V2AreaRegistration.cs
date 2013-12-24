e2068694b322ab89a4f2f7eaed213b45e5322634
Ryan Conrad <camalot@gmail.com>
2013-12-23 23:25:15 -0600

    Upgraded to MVC5
    Reworked the sample site.
    deleted the old sample site.
    new sample site is mvc5

---
+++ EndpointMvc/EndpointMvcSite/Areas/V2/V2AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace EndpointMvcSite.Areas.V2
+{
+    public class V2AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "V2";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "V2_default",
+                "api/v2/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ EndpointMvc/EndpointMvcSite/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EndpointMvcSite.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index ( ) {
+			return View ( );
+		}
+
+		[ChildActionOnly]
+		public PartialViewResult Gist ( String id ) {
+			return PartialView ( model: id );
+		}
+	}
+}
