bd5368a2a0f179e714622cf659d0bd66cd655817
Ryan Conrad <camalot@gmail.com>
2013-12-30 12:49:35 -0600

    added comments to the endpointmvc js script

--- EndpointMvc/EndpointMvcSite/assets/scripts/endpointmvc.js
+++ EndpointMvc/EndpointMvcSite/assets/scripts/endpointmvc.js
@@ -1,6 +1,7 @@
 ﻿(function ($, mouseTrap) {
 	"use strict";
 	$(function () {
+		// wire up the popovers
 		$("[data-toggle='popover']").each(function () {
 			var self = $(this);
 			if (self.data("popover-setup")) {
@@ -10,6 +11,7 @@
 			self.popover();
 		});
 
+		// toggle the open/close
 		$(".epm-nav-toggle").on("click", function () {
 			var self = $(this);
 			var target = $(self.data("epm-target"));
@@ -25,6 +27,7 @@
 			self.trigger("click.epm-mousetrap");
 		});
 
+		// filter the navigation with what is typed in the search field
 		$(".epm-nav .nav-search input.search-field").on("keyup reset.epm-search", function (event) {
 			var self = $(this);
 			var $list = $(self).closest("ul.nav");
@@ -41,6 +44,8 @@
 				}
 			});
 		});
+
+		// this clears the search box and resets the navigation
 		$(".epm-nav .nav-search button.clear").on("click", function (event) {
 			$(".epm-nav .nav-search input.search-field").val("").trigger("reset.epm-search");
 		}).on("show-clear.epm-search",function(event){
@@ -49,7 +54,9 @@
 			$(this).addClass("hidden");
 		});
 
+		// set up scrollspy on the body
 		$("body").scrollspy({ target: ".epm-nav .nav", offset: 100 });
+
 		// smooth scrolling if the jquery scrollTo plugin exists.
 		$(".epm-nav .nav li a[data-epm-scroll]").each(function() {
 			var self = $(this);
@@ -76,6 +83,7 @@
 			var currentPH = field.attr("placeholder");
 			field.attr("placeholder", currentPH + " (Ctrl+Q)");
 
+			// change the title of open/close with ctrl+left/ctrl+right on the click trigger
 			$(".epm-nav-toggle").on("click.epm-mousetrap", function () {
 				$(".epm-nav.expanded.left .epm-nav-toggle").attr("title", "Close (Ctrl+Left)");
 				$(".epm-nav.collapsed.right .epm-nav-toggle").attr("title", "Open (Ctrl+Left)");
@@ -83,13 +91,15 @@
 				$(".epm-nav.expanded.right .epm-nav-toggle").attr("title", "Close (Ctrl+Right)");
 			}).trigger("click.epm-mousetrap");
 
-
 			mouseTrap.bind("ctrl+q", function (e) {
+				// bind ctrl+q to focus the search box
 				$(".epm-nav.expanded .nav-search input.search-field").focus();
 			}).bind("ctrl+left",function(e) {
-				$(".epm-nav.expanded.left .epm-nav-toggle").trigger("click").attr("title","Open (Ctrl+Right)");
+				// bind ctrl+left and either open or close the navigation (depending on the location)
+				$(".epm-nav.expanded.left .epm-nav-toggle").trigger("click").attr("title", "Open (Ctrl+Right)");
 				$(".epm-nav.collapsed.right .epm-nav-toggle").trigger("click").attr("title", "Close (Ctrl+Right)");
-			}).bind("ctrl+right",function(e){
+			}).bind("ctrl+right", function (e) {
+				// bind ctrl+right and either open or close the navigation (depending on the location)
 				$(".epm-nav.expanded.right .epm-nav-toggle").trigger("click").attr("title", "Open (Ctrl+Left)");
 				$(".epm-nav.collapsed.left .epm-nav-toggle").trigger("click").attr("title", "Close (Ctrl+Left)");
 			});
