c4b41ebbba508f006a7fd010661521a7c8ac6006
Ryan Conrad <camalot@gmail.com>
2014-01-25 21:13:48 -0600

    Added an "offline" file for when updating. added static registration for svg and woff files

--- EndpointMvc/Build/nuget.bat
+++ EndpointMvc/Build/nuget.bat
@@ -9,7 +9,6 @@ msbuild Build.msbuild /p:Project=EndpointMvc /verbosity:Diagnostic
 
 REM this will only build the package
 msbuild NugetPublish.msbuild /p:PublishMode=NuGet /p:Project=EndpointMvc.Mvc3 /p:ProjectSharedName=EndpointMvc /p:NuSpecFile=EndpointMvc.Mvc3.nuspec /p:LocalDeploy=E:\Development\deploy\nuget\ /verbosity:Diagnostic
-pause
 msbuild NugetPublish.msbuild /p:PublishMode=NuGet /p:Project=EndpointMvc.Mvc4 /p:ProjectSharedName=EndpointMvc /p:NuSpecFile=EndpointMvc.Mvc4.nuspec /p:LocalDeploy=E:\Development\deploy\nuget\ /verbosity:Diagnostic
 msbuild NugetPublish.msbuild /p:PublishMode=NuGet /p:Project=EndpointMvc.Mvc5 /p:ProjectSharedName=EndpointMvc /p:NuSpecFile=EndpointMvc.Mvc5.nuspec  /p:LocalDeploy=E:\Development\deploy\nuget\ /verbosity:Diagnostic
 REM this is the "reference package" because I changed the nuget id.
